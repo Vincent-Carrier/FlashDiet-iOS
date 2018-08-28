ecb97d9051fa670e54d586a6136a6e581aef83ba
Vincent-Carrier <vincentcarrier15@gmail.com>
2018-08-28 19:25:30 -0400

    Initial Commit

---
+++ FlashDiet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashDiet
+//
+//  Created by Vincent on 2018-08-28.
+//  Copyright © 2018 Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
